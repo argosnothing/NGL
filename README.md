536893a7b5f5cb95eac2ce56390e2adaef756fd1
salivala <argosnothing@gmail.com>
2026-02-06 15:28:35 -0500

    initial env setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## Nix Global Lookup
+
+NGL 
---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "NGL - Nix Global Lookup";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+            cargo
+            rustc
+            rust-analyzer
+            rustfmt
+            clippy
+            
+            # SQLite for rusqlite
+            sqlite
+            
+            # OpenSSL for reqwest
+            openssl
+            pkg-config
+          ];
+
+          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+          
+          shellHook = ''
+            echo "NGL development environment"
+            echo "Rust version: $(rustc --version)"
+          '';
+        };
+      }
+    );
+}
