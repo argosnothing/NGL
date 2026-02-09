88b44e01bb2bed1cb99b87fb16ab1631e6bcc112
salivala <argosnothing@gmail.com>
2026-02-08 22:18:45 -0500

    added lib for interacting with NGL through an interface. TO BE BUILT!!!

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+mod db;
+mod providers;
+mod registry;
+pub mod schema;
+
+pub use schema::{
+    ExampleData, FunctionData, GuideData, NGLData, NGLDataKind, NGLDataVariant, NGLRaw, NGLRequest,
+    NGLResponse, OptionData, PackageData, TypeData,
+};
+
+use db::services::services::query_data;
+use registry::ProviderRegistry;
+use sea_orm::DbErr;
+
+pub async fn query(request: NGLRequest) -> Result<Vec<NGLResponse>, DbErr> {
+    let database_url =
+        std::env::var("DATABASE_URL").unwrap_or_else(|_| "sqlite://ngl.db?mode=rwc".to_string());
+
+    let db = db::establish_connection(&database_url).await?;
+
+    ProviderRegistry::sync(&db, request.clone()).await?;
+
+    let response = query_data(&db, &request).await?;
+
+    Ok(response)
+}
